607f970b6f705ad34b1739017c7cdbf918cefe6f
Taras Khmelnychyi <tarasvirell@gmail.com>
2021-06-10 16:29:25 +0300

    Changed NotFoundPage

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -1,26 +1,24 @@
-import {Button, Container, Row} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 
 import './NotFoundPage.css';
 import Footer from "../components/Footer";
-import {useHistory} from "react-router-dom";
 
 const NotFoundPage = () => {
-    const history = useHistory();
     return (
         <>
             <Container className="not-found-wrapper">
                 <Row className="not-found-text">
-                    404 Page Not Found
+                    404 Сторінку не знайдено
                 </Row>
-                <Row className="not-found-button-wrapper">
-                    <Button
-                        variant="secondary"
-                        className="not-found-button"
-                        onClick={() => {history.goBack();}}
-                    >
-                        Повернутися
-                    </Button>
-                </Row>
+                {/*<Row className="not-found-button-wrapper">*/}
+                {/*    <Button*/}
+                {/*        variant="secondary"*/}
+                {/*        className="not-found-button"*/}
+                {/*        onClick={() => {history.goBack();}}*/}
+                {/*    >*/}
+                {/*        Повернутися*/}
+                {/*    </Button>*/}
+                {/*</Row>*/}
             </Container>
 
             <Footer/>
